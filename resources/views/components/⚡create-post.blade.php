ce6fefd71c46375faf9e4ead1525870b329f57f1
marim <mariamemampp00@gmail.com>
2026-04-07 15:26:59 +0200

    make create component

---
+++ resources/views/components/⚡create-post.blade.php
@@ -0,0 +1,27 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    <h2>New Post:</h2>
+
+    <form >
+        <label>
+            <span>Title</span>
+            <input type="text" >
+        </label>
+
+        <label>
+            <span>Content</span>
+            <textarea ></textarea>
+        </label>
+
+        <button type="submit">Save</button>
+    </form>
+</div>

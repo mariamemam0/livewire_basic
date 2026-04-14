41d7d257eada4cead31466609e75eaef93a3211e
marim <mariamemampp00@gmail.com>
2026-04-14 15:31:40 +0200

    use actions in livewire

--- resources/views/components/⚡greeter.blade.php
+++ resources/views/components/⚡greeter.blade.php
@@ -4,11 +4,41 @@ use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $name = 'Jeremy';
+    public $newName = '';
+
+    public function changeName()
+    {
+        $this->name = $this->newName;
+    }
+
+
 };
 ?>
-
 <div>
-    hello m
+    <div>
+        Hello, {{ $name }}!
+    </div>
+    <form
+    wire:submit="changeName"
+    >
 
+    <div class="mt-2">
+        <input
+            id="newName"
+            type="text"
+            wire:model="newName"
+            class="block w-full p-4 border rounded-md bg-gray-700 text-white"
+        >
+    </div>
+
+    <div class="mt-2">
+        <button
+            type="submit"
+            class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
+\        >
+            Greet
+        </button>
+    </div>
+    </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/','to_do');
+Route::livewire('/','greeter');
 Route::livewire('/counter','counter');
 Route::livewire('/posts','show-posts');
 Route::livewire('/posts/create','create-post');
